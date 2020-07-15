e5d0888490bf99c6aa2d2310dbcbb239f2d9c259
Benjamin Tamasi <halftome@gmail.com>
2020-07-15 11:48:37 +0200

    search

--- simple_form/main.py
+++ simple_form/main.py
@@ -2,7 +2,7 @@ from typing import List
 
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from pony.orm import db_session
+from pony.orm import db_session, select, set_sql_debug
 
 from . import db
 from .deps import paginator, Sorter
@@ -20,9 +20,12 @@ app.add_middleware(
 
 
 @app.get("/", response_model=List[Person])
-def list_people(pagination: Pagination = Depends(paginator), sort: Sorter = Depends()):
+def list_people(pagination: Pagination = Depends(paginator), sort: Sorter = Depends(), search: str = ''):
     with db_session:
-        q = db.order_by(db.Person.select(), sort.dir, sort.order_by).page(pagination.page, pagination.size)
+        set_sql_debug(True)
+        q = select(p for p in db.Person if search in p.name or search in p.email)
+        q = db.order_by(q, sort.dir, sort.order_by).page(pagination.page, pagination.size)
+
         return [p.to_dict() for p in q]
 
 
